2ad25bb9e62d29232392a8a84bfe4db9e458d0ca
OmarDavid464 <garcia.flores.omar.46@cbtis118.edu.mx>
2025-12-05 11:54:12 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,11 +1,16 @@
 <?php
-    $conexion = mysqli_connect(
-"sql100.infinityfree.com",
-"if0_40590712",
-"Oscar313016",
-"if0_40590712_XXX"
+    $conexion=mysqli_connect("sql100.infinityfree.com","if0_40590712","Oscar313016","if0_40590712_omar",
 );
+
 if(!$conexion){
-    die("Error de conexion: " . mysql_connect_error());
+    echo "Error: No se pudo conecetar a MYSQL.";
+    echo "errno de depuracion: ".mysqli_connect_errno();
+    echo "error de depuracion: ".mysqli_connect_error();
+    exit;
 }
+
+echo "Exito: Se realizo la conexion apropiada a MYSQL!.";
+echo "<br>";
+echo "Informacion de host: ". mysqli_get_host_info($conexion);
+mysqli_close($conexion);
 ?>
